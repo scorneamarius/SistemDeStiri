0d537a420e899ac7a73bda1bc028b82a1d398da0
Scornea Alfred Marius <scorneaalfredmarius@yahoo.com>
2020-12-02 21:33:58 +0200

    get list of topics

--- Consumer.java
+++ Consumer.java
@@ -1,31 +1,33 @@
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQTopic;
 
 import javax.jms.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 import java.net.http.WebSocket;
+import java.util.Set;
 
 public class Consumer {
-    public static void main(String []args)throws JMSException {
+    public static void main(String []args) throws JMSException, URISyntaxException, IOException {
         String url = ActiveMQConnection.DEFAULT_BROKER_URL;
-        String queueName = "MESSAGE_QUEUE";
-        System.out.println(url);
-
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
-        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue(queueName);
-        MessageConsumer consumer = session.createConsumer(destination);
-        TextMessage message;
-
-        Listener myListener = new Listener();
-        consumer.setMessageListener(myListener);
+        ActiveMQConnection connection = ActiveMQConnection.makeConnection(url);
         connection.start();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        Set<ActiveMQTopic> allTopics = connection.getDestinationSource().getTopics();
+        System.out.println("All topics are :");
+        for (ActiveMQTopic topic : allTopics){
+            System.out.println(topic.getTopicName());
         }
-        // connection.close();
+        System.out.println("Type topic which you want to read");
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        String topicName = keyboard.readLine();
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createTopic(topicName);
+        MessageConsumer consumer = session.createConsumer(destination);
+        consumer.setMessageListener(new Listener());
+//       connection.close();
+
     }
 }
--- Producer.java
+++ Producer.java
@@ -1,27 +1,38 @@
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 
 import javax.jms.*;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.Set;
 
 public class Producer {
-    public static void main(String []args) throws JMSException
-    {
+    public static void main(String []args) throws JMSException, URISyntaxException, IOException {
         String url = ActiveMQConnection.DEFAULT_BROKER_URL;
-        String queueName = "MESSAGE_QUEUE";
-        System.out.println(url);
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Connection connection = connectionFactory.createConnection();
+        ActiveMQConnection connection = ActiveMQConnection.makeConnection(url);
         connection.start();
-        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue(queueName);
-
-        MessageProducer producer = session.createProducer(destination);
-        TextMessage message = session.createTextMessage("It works");
-        for(int i=0;i<5;i++)
-        {
-            TextMessage messages = session.createTextMessage(message.getText()+ i);
-            producer.send(messages);
+        Set<ActiveMQTopic> allTopics = connection.getDestinationSource().getTopics();
+        System.out.println("All topics are :");
+        for (ActiveMQTopic topic : allTopics){
+            System.out.println(topic.getTopicName());
         }
+        System.out.println("Select the topic where you want to write OR type -> new topic to create a new topic");
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        String topicName = keyboard.readLine();
+        if(topicName.equals("new topic")){
+            System.out.println("Type the name of topic");
+            topicName = keyboard.readLine();
+        }
+        Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+        Destination destination = session.createTopic(topicName);
+        MessageProducer producer = session.createProducer(destination);
+        System.out.println("Type the message");
+        String textMessage = keyboard.readLine();
+        TextMessage message = session.createTextMessage(textMessage);
+        producer.send(message, javax.jms.DeliveryMode.PERSISTENT, javax.jms.Message.DEFAULT_PRIORITY, Message.DEFAULT_TIME_TO_LIVE);
+        session.close();
         connection.close();
     }
 
